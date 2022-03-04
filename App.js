e57031ee48a03ccc9aa9ec1d8f8c8af1cb7aa166
markok <markok@markoks-MacBook-Pro.local>
2022-03-04 14:12:23 +0100

    initial

---
+++ App.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { TouchableOpacity, SafeAreaView, StyleSheet, Text, TextInput } from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <SignUp />
+    </SafeAreaView>
+  )
+}
+
+const SignUp = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordAsterix, setPasswordAsterix] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  /*
+  React.useEffect(() => {
+    //console.log("email", email);
+  }, [email])
+  */
+
+  React.useEffect(() => {
+    let prevPass = password;
+    let newPass = prevPass.replace(/./g, "*");
+    setPasswordAsterix(newPass);
+  }, [password])
+
+  React.useEffect(() => {
+    console.log("passwordAsterix", passwordAsterix);
+  }, [passwordAsterix])
+
+
+  const signUp = () => {
+    console.log("signUp()");
+    setShowPassword(!showPassword);
+  }
+
+  const changePass = ({nativeEvent: {key: keyValue}}) => {
+    let temp;
+
+    if (keyValue === "Backspace") {
+      temp = password.slice(0,-1);
+    }
+    else {
+      temp = password + keyValue;
+    }
+
+    setPassword(temp);
+  }
+
+  return (
+    <SafeAreaView>
+      <Text style={{padding: 20, fontWeight: 'bold', color: 'black'}}>SIGN UP</Text>
+      <Text style={{padding: 20}}>Email</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setEmail}
+        value={email}
+      />
+      <Text style={{padding: 20}}>Password</Text>
+      <TextInput
+        style={styles.textInput}
+        autoCorrect={false}
+        value={password}
+        onKeyPress={changePass}
+        value={showPassword ? password : passwordAsterix}
+      />
+      <TouchableOpacity style={styles.signUpButton} onPress={() => {signUp()}}>
+        <Text style={styles.signUpButtonText}>SIGN UP</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    padding: 20,
+    borderRadius: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginHorizontal: 20
+  },
+  signUpButton: {
+    padding: 20,
+    margin: 20,
+    borderRadius: 10,
+    borderColor: 'red',
+    borderWidth: 1,
+    backgroundColor: 'red'
+  },
+  signUpButtonText: {
+    alignSelf: 'center',
+    color: 'white',
+    fontWeight: 'bold'
+  }
+});
+
+export default App;
